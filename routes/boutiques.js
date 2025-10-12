f6228e4dd9cdeee2e7399bff22d6dbcfac228367
Frankyy92 <fr.harcaut@outlook.fr>
2025-10-13 00:28:06 +0200

    Delete routes directory

--- routes/boutiques.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('dashboard_boutique', { title: 'Boutique - Dashboard' });
-});
-
-module.exports = router;
--- routes/labo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('dashboard_labo', { title: 'Labo - Dashboard' });
-});
-
-module.exports = router;
