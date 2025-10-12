4f84218cd6027e5c1d8666e236fe6948b8eff999
Frankyy92 <fr.harcaut@outlook.fr>
2025-10-12 18:15:07 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-module.exports = {
-  port: process.env.PORT || 3000,
-  sessionSecret: process.env.SESSION_SECRET || 'dev_secret_change_me',
-  timezone: process.env.TZ || 'Europe/Paris',
-  cutoffHour: 21, // 21:00 local
-};
